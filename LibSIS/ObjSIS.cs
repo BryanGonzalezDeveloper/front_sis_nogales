e790ccc6b0a68968fc42309f4a38d2925aac760d
Bryan Enrique Gonzalez Zambrano <63970786+BryanGonzalezDeveloper@users.noreply.github.com>
2022-10-22 22:36:02 -0700

    Add project files.

---
+++ LibSIS/ObjSIS.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibSIS
+{
+    public class ObjSIS
+    {
+        public class Articulo
+        {
+            public int intID;
+            public string Descripcion;
+            public decimal precioUnitario;
+            public int stockActual;
+            public int IdProveedor;
+            public int stockMinimo;
+
+            public string InsertarCita(string strconexion)
+            {
+                string striSQL = $"INSERT INTO [dbo].[Articulo] ([Descripcion],[precioUnitario],[stockActual] ,[IdProveedor] ,[stockMinimo] ) VALUES (" +
+                    $",'{Descripcion}', {precioUnitario}, {stockActual},{IdProveedor},{stockMinimo})";
+                string strError = "";
+                int intRegistrosAfectados;
+                intRegistrosAfectados = cslUtileriasBD.clsSQLServer.exeQuery(strconexion, striSQL, ref strError);
+               
+                if (strError == "")
+                {
+                    return "";
+                }
+                else
+                {
+                    return strError;
+                }
+            }
+        }
+        public class Proveedor
+        {
+            public string razonSocial;
+            public string Telefono;
+            public string email;
+            public string sitioWeb;
+
+            public string InsertarCita(string strconexion)
+            {
+                string striSQL = $"INSERT INTO [dbo].[Proveedor]([razonSocial],[Telefono],[email],[sitioWeb])" +
+                    $"VALUES( '{razonSocial}', '{Telefono}', '{email}', '{sitioWeb}')";
+                string strError = "";
+                int intRegistrosAfectados;
+                intRegistrosAfectados = cslUtileriasBD.clsSQLServer.exeQuery(strconexion, striSQL, ref strError);
+
+                if (strError == "")
+                {
+                    return "";
+                }
+                else
+                {
+                    return strError;
+                }
+            }
+        }
+    }
+}
---
+++ ObjSIS/Class1.cs
@@ -0,0 +1,32 @@
+﻿namespace ObjSIS
+{
+    public class ObjSIS
+    {
+        public class Articulo
+        {
+            public int intID;
+            public string Descripcion;
+            public decimal precioUnitario;
+            public int stockActual;
+            public int IdProveedor;
+            public int stockMinimo;
+
+            public string InsertarCita(string strconexion)
+            {
+                string striSQL = $"INSERT INTO [dbo].[TbCita]([Articulo],[ID],[Descripcion],[precioUnitario],[stockActual] ,[IdProveedor] ,[stockMinimo] )VALUES ({intID}" +
+                    $",'{Descripcion}', { precioUnitario}, { stockActual},'{IdProveedor}','{stockMinimo}')";
+                string strError = "";
+                int intRegistrosAfectados;
+                intRegistrosAfectados = cslUtileriasBD.clsSQLServer.exeQuery(strconexion, striSQL, ref strError);
+                if (strError == "")
+                {
+                    return "";
+                }
+                else
+                {
+                    return strError;
+                }
+            }
+            }
+        }
+    }
